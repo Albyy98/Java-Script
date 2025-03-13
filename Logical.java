a6318fdc6f67f6dc3f0b1c1d65a52ef2f5e51a89
Albyy98 <albinjosephantony82@gmail.com>
2025-03-13 13:34:37 +0530

    hello

--- Logical.java
+++ Logical.java
@@ -1,8 +1,8 @@
 public class Logical{
     public static void main(String []args){
-        int A=10;
+        int A=40;
         int B=20;
-        boolean result = (A>B)&&(A==B);
-        System.out.println("(A<B)&&(A!=B):"+result);
+        boolean i=(A>B)||(A==B);
+        System.out.println("(A<B)||(A!=B):"+i);
     }
 }
