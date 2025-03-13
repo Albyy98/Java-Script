9b5b2fe26c4d90d74bc5b67ebdaeb18882c65903
Albyy98 <albinjosephantony82@gmail.com>
2025-03-13 12:08:57 +0530

    Example program 5

---
+++ Logical.java
@@ -0,0 +1,8 @@
+public class Logical{
+    public static void main(String []args){
+        int A=10;
+        int B=20;
+        boolean result = (A>B)&&(A==B);
+        System.out.println("(A<B)&&(A!=B):"+result);
+    }
+}
