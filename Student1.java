0d7a6c809ae8473855d1ead85a3c37edd9bf85b8
Albyy98 <albinjosephantony82@gmail.com>
2025-03-13 10:08:22 +0530

    Next Example Program

---
+++ Student1.java
@@ -0,0 +1,12 @@
+public class Student1{
+    public static void main(String []args){
+    String name="Albin";
+    String SubjectName="Java";
+    int Mark=99;
+    String Grade="O";
+    System.out.println("The Student name is:" +name);
+    System.out.println("The Subject is:" +SubjectName);
+    System.out.println("The mark is:" +Mark);
+    System.out.println("The grade is:" +Grade);
+    }
+}
